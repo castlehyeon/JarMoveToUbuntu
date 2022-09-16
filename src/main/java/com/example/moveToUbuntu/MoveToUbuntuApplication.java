c20fe2b2dc55efe338ad117bcd3924d386edcb37
castlehyeon <qnftktls118@naver.com>
2022-09-16 09:29:43 +0900

    settings

---
+++ src/main/java/com/example/moveToUbuntu/MoveToUbuntuApplication.java
@@ -0,0 +1,13 @@
+package com.example.moveToUbuntu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveToUbuntuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveToUbuntuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moveToUbuntu/MoveToUbuntuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moveToUbuntu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveToUbuntuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
